2467178d227928e74e86abdc34ec834128c55d43
SpreadSheets600 <sohammaity006@gmail.com>
2025-06-04 22:35:17 +0530

    feat: added main fucntions

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from vidbot!")
+from src.logger import get_logger
+from src.script.generator import generate_script
+from src.reddit.scrapper import scrape_subreddit_data
 
+logger = get_logger("VidBot")
 
-if __name__ == "__main__":
-    main()
+logger.header("Welcome To Vid Bot")
+logger.separator()
+
+logger.info("Scrapping Off Subreddit To Get Data")
+
+try:
+    scrape_subreddit_data(subreddit_name="askreddit", limit=1)
+    logger.success("Done! Moving To Next Setp")
+
+except Exception as e:
+    logger.error(f"Error Occured While Scrapping The Subreddit : {e}")
+
+logger.info("Using Gemini To Generate Script")
+
+try:
+    generate_script()
+    logger.success("Done! Moving To Next Step")
+except Exception as e:
+    logger.error(f"Error Occured While Generating Script With Gemini : {e}")
